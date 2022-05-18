e2c8cda0f35ea08137f7dae81f0d1954edaed3b1
Prabhat <prabhatkumar944@gmail.com>
2022-05-18 13:42:03 +0530

    first currying project

---
+++ main.js
@@ -0,0 +1,27 @@
+//ONE WAY TO USE CURRYING
+
+// let multiply = function (x , y){
+//     console.log(x * y);
+// }
+
+// let multiplyByTwo = multiply.bind(this, 2);
+// multiplyByTwo(3);
+
+// let multiplyByThree = multiply.bind(this, 3);
+// multiplyByThree(10);
+
+
+
+//ANOTHER WAY OF USING CURRYING
+
+var multiply = function (x){
+    return function(y){
+        console.log(x * y);
+    }
+}
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(3);
+
+let multiplyByThree = multiply(3);
+multiplyByThree(10);
