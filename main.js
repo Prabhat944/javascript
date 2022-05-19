e22c2576837cd4ad3da04859b3c766793700711b
Prabhat <prabhatkumar944@gmail.com>
2022-05-19 17:22:14 +0530

    student class with eligiblity for placement project

--- main.js
+++ main.js
@@ -1,27 +1,58 @@
-//ONE WAY TO USE CURRYING
-
-// let multiply = function (x , y){
-//     console.log(x * y);
-// }
-
-// let multiplyByTwo = multiply.bind(this, 2);
-// multiplyByTwo(3);
-
-// let multiplyByThree = multiply.bind(this, 3);
-// multiplyByThree(10);
-
-
-
-//ANOTHER WAY OF USING CURRYING
-
-var multiply = function (x){
-    return function(y){
-        console.log(x * y);
+class Student{
+    static no_of_student=0;
+    constructor(name,age,phone_number,board_marks){
+  
+     this.name=name,
+  
+     this.age=age,
+  
+     this.phone_number=phone_number,
+  
+     this.board_marks=board_marks
+     Student.student_count();
+  
+    };
+   static student_count(){
+        return Student.no_of_student++;
+    }
+eligible_for_placements(required_marks){
+    
+    return (min_age) => {
+        if(this.age >= min_age && this.board_marks >= required_marks){
+           console.log(`${this.name} is eligible for placement`);
+        }
+        else{
+            console.log(`${this.name} is not eligible`);
+        }
     }
 }
+  eligible(){
+  
+             if(this.board_marks >= 40) 
+                console.log(`${this.name} is eligible for registration`);    
+              else 
+                console.log(`${this.name} is not eligible for registration`);
+            }
+  }
+  const mohan=new Student('mohan',20,32457609,45);
+  mohan.eligible();
 
-let multiplyByTwo = multiply(2);
-multiplyByTwo(3);
+  const ajay=new Student('ajay',25,987987564,39);
+  ajay.eligible();
 
-let multiplyByThree = multiply(3);
-multiplyByThree(10);
+  const prabhat=new Student('prabhat',26,987987564,75);
+ prabhat.eligible();
+
+  const yogi=new Student('yogi',24,987987564,40);
+  yogi.eligible();
+
+  const rahul=new Student('rahul',27,987987564,20);
+  rahul.eligible();
+
+ console.log(Student.student_count());
+
+mohan.eligible_for_placements(45)(20); 
+ajay.eligible_for_placements(45)(20);
+prabhat.eligible_for_placements(45)(20);
+yogi.eligible_for_placements(45)(20);
+rahul.eligible_for_placements(45)(20);
